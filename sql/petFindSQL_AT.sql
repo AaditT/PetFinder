060c28530178ee464a963f179c76e384622f2f3c
Aadit Trivedi <aadit.trivedi03@gmail.com>
2020-12-31 23:36:16 -0800

    Update petFindSQL_AT.sql

--- sql/petFindSQL_AT.sql
+++ sql/petFindSQL_AT.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pet_find`
+-- Database: `petfinder_AT`
 --
 
 -- --------------------------------------------------------
